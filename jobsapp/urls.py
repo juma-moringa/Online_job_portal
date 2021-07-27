04bb152beda7060907c3a803c3ae413ca6cce340
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-27 17:17:52 +0300

    link up the navigation bar

--- jobsapp/urls.py
+++ jobsapp/urls.py
@@ -13,7 +13,6 @@ urlpatterns=[
     path('login/',views.login_request,name = 'login'),
     path('logout/',views.logout_request,name = 'logout'),
 
-  
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
